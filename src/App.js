5af3cce2bc7b83710f363b921d55ed300a1ebdcc
Giancarlo <g.uzzo@infcom.it>
2021-01-01 21:51:26 +0100

    add header

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 
-
 function App() {
   return (
     <>
       <Navbar />
       <Header />
-      <Particles />
     </>
   );
 }
